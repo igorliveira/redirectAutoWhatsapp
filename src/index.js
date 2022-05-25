c86431cd5d0e229783d9903bd5632484232ed0eb
igorliveira <igor.0208.oliveira@gmail.com>
2022-05-25 12:23:06 -0300

    att2

--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ const routes = require('./routes');
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.SERVERPORT || 3000, () => console.log("Servidor Rodando!"));
+app.listen(process.env.PORT || 3000, () => console.log("Servidor Rodando!"));
